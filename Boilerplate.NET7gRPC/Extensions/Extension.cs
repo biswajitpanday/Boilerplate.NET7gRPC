deeee111b925d8e4c5cbce44f15a856595b697da
Biswajit Panday <biswajitmailid@gmail.com>
2022-12-01 03:49:00 +0600

    Add Open API Specification using gRPC.Swagger and make Swagger url the main api url.

---
+++ Boilerplate.NET7gRPC/Extensions/Extension.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Boilerplate.NET7gRPC.Extensions;
+
+public static class Extension
+{
+
+    #region MiddleWare Configure
+    /// <summary>
+    /// Configure Swagger for this application
+    /// </summary>
+    /// <param name="services"></param>
+    public static void AppRegisterSwagger(this IServiceCollection services)
+    {
+        services.AddGrpcSwagger();
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1",
+                new OpenApiInfo
+                    { Title = "Hedwig Server", Version = "v1", Description = "Boilerplate .NET7 Server Using gRPC" });
+        });
+    }
+
+    #endregion
+
+
+
+    #region MiddleWare Use
+
+    /// <summary>
+    /// Use Swagger and make Swagger url the main api url
+    /// </summary>
+    /// <param name="app"></param>
+    public static void AppUseSwagger(this WebApplication app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "Boilerplate .NET7 Server Using gRPC");
+            options.RoutePrefix = string.Empty;
+        });
+    }
+
+    #endregion
+}
--- Boilerplate.NET7gRPC/Program.cs
+++ Boilerplate.NET7gRPC/Program.cs
@@ -1,3 +1,4 @@
+using Boilerplate.NET7gRPC.Extensions;
 using Boilerplate.NET7gRPC.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,10 +10,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddGrpc();
 builder.Services.AddGrpcReflection();
 builder.Services.AddGrpc().AddJsonTranscoding();
+builder.Services.AppRegisterSwagger();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.AppUseSwagger();
 app.MapGrpcService<GreeterService>();
 app.MapGrpcReflectionService();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
