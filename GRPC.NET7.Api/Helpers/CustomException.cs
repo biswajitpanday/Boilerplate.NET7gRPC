bc507e38e863cfed4e19067b763a3575a7bb2fbf
Biswajit Panday <biswajitmailid@gmail.com>
2022-12-09 04:27:56 +0600

    Create CustomException Type.

---
+++ GRPC.NET7.Api/Helpers/CustomException.cs
@@ -0,0 +1,10 @@
+﻿namespace GRPC.NET7.Api.Helpers
+{
+    public class CustomException : Exception
+    {
+        public CustomException(string message) : base(message)
+        {
+            
+        }
+    }
+}
