ee365d01dd1cc8c9283576b699c92d26daf0eab6
Biswajit Panday <biswajitmailid@gmail.com>
2022-12-09 18:59:25 +0600

    Refactor CustomMapper. Add Datetime type proto to test CustomMapper.

--- GRPC.NET7.Api/Helpers/CustomMapper.cs
+++ GRPC.NET7.Api/Helpers/CustomMapper.cs
@@ -7,29 +7,28 @@ public static class CustomMapper
     /// </summary>
     /// <typeparam name="TSource"></typeparam>
     /// <typeparam name="TDestination"></typeparam>
-    /// <param name="srcValue"></param>
-    /// <returns></returns>
-    public static TDestination Map<TSource, TDestination>(TSource srcValue) where TDestination: new()
+    /// <param name="src"></param>
+    /// <returns>TDestination</returns>
+    public static TDestination Map<TSource, TDestination>(TSource src) where TDestination: new()
     {
         try
         {
-            var destType = typeof(TDestination);
-            var tDest = (TDestination)Activator.CreateInstance(destType)!;
-            if (srcValue == null)
+            var tDest = (TDestination)Activator.CreateInstance(typeof(TDestination))!;
+            if (src == null)
                 return tDest;
-            var srcClassType = srcValue.GetType();
+            var srcClassType = src.GetType();
             var srcProperties = srcClassType.GetProperties();
             foreach (var srcProperty in srcProperties)
             {
                 var destPropertyInfo = tDest.GetType().GetProperty(srcProperty.Name);
                 if (srcProperty.GetType() == destPropertyInfo?.GetType())
-                    destPropertyInfo?.SetValue(tDest, srcProperty.GetValue(srcValue, null), null);
+                    destPropertyInfo.SetValue(tDest, srcProperty.GetValue(src, null), null);
             }
             return tDest;
         }
         catch (Exception e)
         {
-            throw new Exception($"Unsupported mapping.\n {e.Message}");
+            throw new Exception($"Unsupported mapping.\n{e.Message}");
         }
     }
 }
--- GRPC.NET7.Core/Entities/UserEntity.cs
+++ GRPC.NET7.Core/Entities/UserEntity.cs
@@ -1,8 +1,12 @@
-﻿namespace GRPC.NET7.Core.Entities;
+﻿using GRPC.NET7.Core.Enums;
+
+namespace GRPC.NET7.Core.Entities;
 
 public class UserEntity : BaseEntity
 {
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public string Email { get; set; } = null!;
+    public DateTime DateOfBirth { get; set; }
+    public AppEnums.Gender Gender { get; set; }
 }
