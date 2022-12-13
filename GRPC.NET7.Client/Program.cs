588bc4918e564b33288da696e45c8222fc7e9a2a
Biswajit Panday <biswajitmailid@gmail.com>
2022-12-14 05:22:19 +0600

    Client / Consumer Project Add.

--- GRPC.NET7.Client/Program.cs
+++ GRPC.NET7.Client/Program.cs
@@ -1,17 +1,2 @@
-using GRPC.NET7.Client.Services;
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-
-// Add services to the container.
-builder.Services.AddGrpc();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-
-app.Run();
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
