853604356248edac8a41ab538c02ba3e396025fc
Biswajit Panday <biswajitmailid@gmail.com>
2022-12-14 06:30:44 +0600

    Client configure for accessing auth.

--- GRPC.NET7.Client/Program.cs
+++ GRPC.NET7.Client/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using Grpc.Net.Client;
+using GRPC.NET7.Client.Protos;
+
 Console.WriteLine("Hello, World!");
+
+var serverAddress = "https://localhost:7166";
+
+using var channel = GrpcChannel.ForAddress(serverAddress);
+
+var authenticationClient = new Authentication.AuthenticationClient(channel);
+var authenticationResponse = authenticationClient.Authenticate(new AuthenticationRequest
+{
+    UserName = "admin",
+    Password = "admin"
+});
+
+Console.WriteLine($"Received Authentication Response - \nToken: {authenticationResponse.AccessToken}\nExpires In: {authenticationResponse.ExpiresIn}");
+
+//var calculationClient = new calculatio
+
+
+Console.ReadKey();
+await channel.ShutdownAsync();
